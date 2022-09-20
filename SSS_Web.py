f80b19a42aa7c9e563d3b741d9ea12d4449bdf4f
KimHaengBok <88387139+KimHaengBok@users.noreply.github.com>
2022-09-20 17:33:47 +0900

    Update SSS_Web.py

--- SSS_Web.py
+++ SSS_Web.py
@@ -37,11 +37,6 @@ def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
 conn = init_connection()
 
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
 
 def main_page():
     st.markdown("# Smart Sensor System")
